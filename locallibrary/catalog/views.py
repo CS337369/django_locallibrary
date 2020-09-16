4adb37a5ae4c61110139c1ecddd595fffd4efd68
ChangSoo <ckdtnek123@kakao.com>
2020-09-17 00:14:29 +0900

    biu done

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from catalog.models import Book, Author, BookInstance, Genre, BookInstanceUpdate
+from catalog.models import Book, Author, BookInstance, Genre
 
 def index(request):
     """View function for home page of site."""
